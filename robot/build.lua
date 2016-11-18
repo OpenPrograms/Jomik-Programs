70a426467d88259ce3201e89d0fbc81e74f5468b
Jonas Damtoft <jonasdamtoft@hotmail.com>
2016-11-18 19:29:55 +0100

    add movement in layer

--- robot/build.lua
+++ robot/build.lua
@@ -1,7 +1,8 @@
 local VERSION = "0.1a"
-local RELATIVE_BUILD_LIB = "lib/build/"
+local BUILD_LIB_DIR = "build/"
 
 local filesystem = require("filesystem")
+local location = require("location")
 local robot = require("robot")
 local shell = require("shell")
 local vector = require("vector")
@@ -9,7 +10,7 @@ local vector = require("vector")
 local args, opts = shell.parse(...)
 
 local root_path = filesystem.path(filesystem.path(os.getenv("_")), "..")
-local lib_dir = filesystem.concat(root_path, RELATIVE_BUILD_LIB)
+local lib_dir = filesystem.concat(root_path, "lib", BUILD_LIB_DIR)
 
 local usage = [[
 Usage:
@@ -35,7 +36,7 @@ elseif opts.l or opts.list then
     print("Available shapes for robot-build:")
     for f in filesystem.list(lib_dir) do
       if not filesystem.isDirectory(f) and string.match(f, "[%a%d]+.lua") then
-          print(f)
+          print(select(3, string.find(f, "([%a%d]+).lua")))
         end
       end
   else
@@ -47,11 +48,13 @@ elseif not opts.shape or opts.h or opts.help then
   return
 end
 
-local shape_file = lib_dir .. opts.shape .. ".lua"
-assert(filesystem.exists(shape_file) and not filesystem.isDirectory(shape_file),
+local shape_file = filesystem.concat(lib_dir, opts.shape .. ".lua")
+assert(filesystem.exists(shape_file)
+  and not filesystem.isDirectory(shape_file)
+  and not filesystem.isLink(shape_file),
   "build file not found (" .. opts.shape .. ")")
 
-local shape_lib = require(lib_dir .. opts.shape)
+local shape_lib = require(filesystem.concat(BUILD_LIB_DIR, opts.shape))
 if opts.i or opts.info then
   shape_lib.info()
   return
@@ -60,11 +63,54 @@ elseif opts.d or opts.dry then
   return
 end
 
-local shape = shape_lib:fromargs(table.unpack(args))
-if not shape then
-  return
+local shape = shape_lib.fromargs(table.unpack(args))
+
+local function next_block()
+  local position, orientation = location.get()
+  local right = vector(-orientation.z, orientation.y, orientation.x)
+  if shape:is_shell(position + orientation) then
+    -- Go forward, place block behind
+    robot.forward()
+    robot.turnAround()
+    -- place block
+    robot.turnAround()
+  elseif shape:is_shell(position + right) then
+    -- turn right, go forward, place block behind
+    robot.turnRight()
+    robot.forward()
+    robot.turnAround()
+    -- place block
+    robot.turnAround()
+  elseif shape:is_shell(position - right) then
+    -- turn left, go forward, place block behind
+    robot.turnLeft()
+    robot.forward()
+    robot.turnAround()
+    -- place block
+    robot.turnAround()
+  else
+    -- turn right, go forward, place block behind, turn left, go forward
+    robot.turnRight()
+    robot.forward()
+    robot.turnAround()
+    -- place block
+    robot.turnRight()
+    robot.forward()
+  end
 end
 
+-- find edge
+local position, orientation = location.get()
+while shape:contains(position + orientation) do
+  robot.forward()
+  position, orientation = location.get()
+end
+
+robot.turnRight()
+
+while true do
+  next_block()
+end  
 
 --[[
 We need a special case for the top and bottom.
--- robot/build/ellipsoid.lua
+++ robot/build/ellipsoid.lua
@@ -6,7 +6,7 @@ ellipsoid.__index = ellipsoid
 local function new(center, radius)
   assert(vector.isvector(center) and vector.isvector(radius), "wrong argument types (expected <vector>")
   return setmetatable({center = center,
-      radius = radius + vector(0.5, 0.5, 0.5)
+      radius = radius + vector(0.5, 0.5, 0.5),
       min = center - radius,
       max = center + radius
       }, ellipsoid)
@@ -14,23 +14,23 @@ end
 local standard = new(vector(0, 0, 0), vector(1, 1, 1))
 
 local function fromargs(cx, cy, cz, rx, ry, rz)
-    return new(
-      vector(
-        tonumber(cx or 0),
-        tonumber(cy or 0),
-        tonumber(cz or 0)),
-      vector(
-        tonumber(rx or 1),
-        tonumber(ry or 1),
-        tonumber(rz or 1)))
-                    
+  return new(
+    vector(
+      tonumber(cx) > 0 and tonumber(cx) or 0,
+      tonumber(cy) > 0 and tonumber(cy) or 0,
+      tonumber(cz) > 0 and tonumber(cz) or 0),
+    vector(
+      tonumber(rx) > 1 and tonumber(rx) or 1,
+      tonumber(ry) > 1 and tonumber(ry) or 1,
+      tonumber(rz) > 1 and tonumber(rz) or 1))
+                  
 end
 
 function ellipsoid:contains(position)
-  return ((position - self.center) / self.cradius):len2() <= 1
+  return ((position - self.center) / self.radius):len2() <= 1
 end
 
-function ellipsoid:isouter(position)
+function ellipsoid:is_shell(position)
   return self:contains(position) and not 
         (self:contains(position + vector(-1, 0, 0))
      and self:contains(position + vector(1, 0, 0))
